ed6d6197e49218a2aa4713058fe480985a623f2a
Abdiel Efrain Morales Gomez <manzanitaspro@gmail.com>
2026-05-04 21:05:35 -0600

    Arregle error de sintaxis

--- funciones/titulo.php
+++ funciones/titulo.php
@@ -1,8 +1,8 @@
 <?php
 include_once "centrar.php";
 
-$titulo = centrar($titulo, $ancho); 
 function titulo(string $titulo, $ancho = 40): void {
+    $titulo = centrar($titulo, $ancho); 
     echo "╔" . str_repeat("═", $ancho) . "╗\n";
     echo "║" .$titulo."║\n";
     echo "╚" . str_repeat("═", $ancho) . "╝\n";
